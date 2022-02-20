5998d5589f4c5bb58129418c34d1457dbe6af65c
loudo1982 <loudo1982@gmail.com>
2022-02-19 20:01:55 -0700

    mofificaciones 2

--- pages/index.js
+++ pages/index.js
@@ -85,7 +85,7 @@ const Index = ()=>{
 Objetivo:
 </h1>
 
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
 El objetivo es una competencia de aplicación de los conocimientos de Cálculo I y Cálculo II en formato torneo respetando el plan analítico de Prepa Tec. 
 
 
@@ -94,42 +94,42 @@ El objetivo es una competencia de aplicación de los conocimientos de Cálculo I
 Reglas: 
 </h1>
 
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white   ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white   ">
 Las inscripciones son abiertas hasta alcanzar un máximo de 32 equipos.
 
 
 
 </p>
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
   Cada equipo asignará a un coach que lo representará, mismos que estarán presentes el día del torneo apoyando al comité organizador.Las o los coaches deben ser profesores de la institución.</p>
 
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white">Los equipos se enfrentarán 1 a 1 en eliminatorias de 15 a 20 minutos, donde obtendremos un ganador que pasará a la siguiente ronda, así sucesivamente hasta obtener un equipo campeón.</p>
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white">Los equipos se enfrentarán 1 a 1 en eliminatorias de 15 a 20 minutos, donde obtendremos un ganador que pasará a la siguiente ronda, así sucesivamente hasta obtener un equipo campeón.</p>
 
 <h1 className="text-2xl font-normal leading-normal mt-0 mb-2 text-white ml-8 underline ">
 A quién está dirigido:
 </h1>
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">Está dirigido a alumnos de preparatoria de Prepa Tec de sexto grado que han cursado las materias de cálculo 1 y cálculo 2.
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">Está dirigido a alumnos de preparatoria de Prepa Tec de sexto grado que han cursado las materias de cálculo 1 y cálculo 2.
 
      </p>
 
      <h1 className="text-2xl font-normal leading-normal mt-0 mb-2 text-white ml-8 underline">Fecha del evento: </h1>
-     <p className=" text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white "> Viernes 22 de Abril de las 4:00 pm ( hora centro). 
+     <p className=" text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white "> Viernes 22 de Abril de las 4:00 pm ( hora centro). 
 La duración del evento es de 2 a 3 horas.
  </p>
 
-     <h1 className="text-2xl font-normal leading-normal mt-0 mb-2 text-white ml-8 underline w-4/6 ">Cierre de inscripciones:
+     <h1 className="text-2xl font-normal leading-normal mt-0 mb-2 text-white ml-8 underline w-5/6 ">Cierre de inscripciones:
  </h1>
-     <p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white "> Viernes 15 de abril a medianoche.
+     <p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white "> Viernes 15 de abril a medianoche.
 </p>
 <h1 className="text-2xl font-normal leading-normal mt-0 mb-2 text-white ml-8 underline">Premios: 
  </h1>
-     <p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">El equipo ganador y el segundo lugar tendrán un buen premio.
+     <p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">El equipo ganador y el segundo lugar tendrán un buen premio.
 </p>
 
 <h1 className="text-2xl font-normal leading-normal mt-0 mb-2 text-white ml-8 underline ">
 Los temas del torneo:
 </h1>
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
  <li>Asíntotas y puntos huecos</li>
 
 <li>análisis de funciones polinomiales y racionales</li>
@@ -140,15 +140,22 @@ Los temas del torneo:
 <li>Optimización</li>
 
      </p>
+     <h1 className="text-2xl font-normal leading-normal mt-0 mb-2 text-white ml-8 underline ">
+Zoom id del torneo:
+</h1>
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white "> El torneo se llevará a cabo por medio de zoom( zoom id 769 910 7319).
+
+     </p>
 <div>
 <h1 className="text-2xl font-normal leading-normal mt-0 mb-2 text-white ml-8 underline ">Organizadores: 
  </h1>
-     <p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white  ">Carlos Aurelio Robles Amador:	PrepaTec Metepec ;		mail: ca.robles.amador@tec.mx 
-<p>Tiberiu Hrabak: PrepaTec Guadalajara	;	mail:thrabak@tec.mx </p>
-<p>Osvaldo Samayoa Ochoa:PrepaTec Chiapas; mail: osvaldo.samayoa@tec.mx </p>
- <p>Ludovic Betton: PrepaTec Sonora Norte ;	mail: ludovic@tec.mx  </p>
- <p className="text-2xl font-light leading-relaxed mt-0 mb-8  pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white  "> Más información al whatsapp: 6624332660</p>
-</p>
+     <p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white  ">Carlos Aurelio Robles Amador:	PrepaTec Metepec ; mail: ca.robles.amador@tec.mx </p>
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white  ">Tiberiu Hrabak: PrepaTec Guadalajara	;	mail:thrabak@tec.mx </p>
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white  ">Osvaldo Samayoa Ochoa:PrepaTec Chiapas; mail: osvaldo.samayoa@tec.mx </p>
+ <p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white  ">Ludovic Betton: PrepaTec Sonora Norte ;	mail: ludovic@tec.mx  </p>
+ 
+
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white  "> Más información al whatsapp: 6624332660</p>
 
 <p className="bg-gray-700">.
 </p></div>
--- pages/reglas.js
+++ pages/reglas.js
@@ -58,27 +58,27 @@ const Reglas = () => {
            Reglas en las partidas:
 </h1>
 
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
 El torneo consiste en 5 rondas para poder llegar a la gran final, cada ronda se jugará de dos preguntas para obtener al equipo ganador. Si existiera empate o juego desierto, el juez pondrá un tercer ejercicio.</p>
 
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
 Cada pregunta tendrá un tiempo de 5 minutos para su solución.</p>
 
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
 Los equipos irán a enfrentarse en breakout room. Les daremos el número de la sala en la cual se enfrentarán cada uno en un momento.  En este breakout room irá también un juez que es un coach de equipo, pero no será su propio coach, los jueces verificarán y darán validez a las respuestas y procedimientos del equipo ganador de cada ronda. Su juicio es inapelable. En la carpeta de ejercicios que les vamos a compartir a los jueces, viene la solución paso a paso de cada ejercicio, les pedimos que se cubra al menos 80% de estos procedimientos para que se pueda declarar ganador un equipo u otro, en la resolución de ejercicios.
 </p>
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
 Con los dos equipos en el breakout room y una vez que el maestro juez lea el primer ejercicio de la ronda 1, se empieza a cronometrar el primer ejercicio, los micrófonos deberán quedar apagados y las cámaras prendidas. Favor de no usar imágenes virtuales cómo fondo de pantalla. Los integrantes de cada equipo podrán trabajar juntos por su lado usando otro medio de comunicación ( WhatsApp , Zoom , Meet Google, Hangout , Teams etc…). Cuando un equipo piense que tiene una buena respuesta ya escrita, entonces un integrante deberá mandar su respuesta al juez por mensaje privado en el chat de zoom. Si la respuesta es correcta, el juez confirma el turno al equipo y pide que le muestre en frente de la cámara el procedimiento hecho. Si el juez considera que el proceso es correcto, el equipo gana el punto y se pasará a la siguiente pregunta. Si el juez encuentra el procedimiento erróneo o no completo, se dará el turno de responder al otro equipo. Si al final de los 5 minutos, el otro equipo no ha respondido entonces el equipo 1 podrá volver a dar una respuesta. Si ningún equipo gana, se procederá a la siguiente pregunta. Si después de 2 preguntas, no hay ganador, se procederá a la pregunta de desempate.
 </p>
 
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
 El juez declarará la respuesta correcta o no y propondrá otro ejercicio si es necesario. 
 Al final de cada ronda, pasará un solo equipo de cada breakout room a la siguiente ronda.
 Los equipos eliminados se podrán retirar al finalizar su participación en la ronda una vez que el juez haya dado su veredicto y el equipo ganador deberá regresar a la sala principal y esperar la siguiente ronda.
 En la ronda semifinal y final, en cada breakout room van a estar 3 jueces, para que las decisiones sean sin la menor duda, de los 4 equipos que están compitiendo.
 
 </p>
-<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-4/6 text-left bg-gray-500 border-2 border-white ">
+<p className="text-2xl font-light leading-relaxed mt-0 mb-4 ml-8 pl-2 text-white w-5/6 text-left bg-gray-500 border-2 border-white ">
 Cualquier situación no contemplada en este documento será analizada por el comité organizador y el juez correspondiente.
 
 </p>
